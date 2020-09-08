56e355121c561b69e04963505103c74033509834
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-09-08 16:34:39 -0500

    home page blog created

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -4,6 +4,32 @@ import './index.scss'
 export default function Home() {
   return (
     <section className='Home'>
+      <article></article>
+      <article>
+        <h1>Blog</h1>
+
+        <div>
+          <img src={process.env.PUBLIC_URL + '/img/blog1.jpg'} alt="working" />
+          <div>
+            <h4>Tips para volver a clases</h4>
+            <p>Haz una lista de todo lo que necesitas comprar y todo lo que necesitas hacer. Te sentirás más realizado y capaz de luchar</p>
+          </div>
+        </div>
+
+        <img src={process.env.PUBLIC_URL + '/img/publicidad.jpg'} alt='publicidad' />
+
+        <div>
+          <img src={process.env.PUBLIC_URL + '/img/blog1.jpg'} alt="working" />
+          <div>
+            <h4>Tips para volver a clases</h4>
+            <p>Haz una lista de todo lo que necesitas comprar y todo lo que necesitas hacer. Te sentirás más realizado y capaz de luchar</p>
+          </div>
+        </div>
+
+        <img src={process.env.PUBLIC_URL + '/img/publicidad-1.jpg'} alt='publicidad' />
+
+      </article>
+      <article></article>
     </section>
   )
 }
