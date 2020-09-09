e24070af8739b5e4f4e3e4d9dc47da067ba11fa5
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-09-09 17:18:05 -0500

    Updates

---
+++ precache-manifest.1d810a3a69f320a74c7f87c3b3005115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74c384d1d8dc0e4104580ef7373b3c4",
+    "url": "/Conectir/index.html"
+  },
+  {
+    "revision": "dde5c69fab7c4743733d",
+    "url": "/Conectir/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "241f064084836251c274",
+    "url": "/Conectir/static/css/main.f3a06dfa.chunk.css"
+  },
+  {
+    "revision": "dde5c69fab7c4743733d",
+    "url": "/Conectir/static/js/2.5b5db04f.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/Conectir/static/js/2.5b5db04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241f064084836251c274",
+    "url": "/Conectir/static/js/main.7741175e.chunk.js"
+  },
+  {
+    "revision": "3edd223e975fa80761ca",
+    "url": "/Conectir/static/js/runtime-main.3596192a.js"
+  }
+]);
