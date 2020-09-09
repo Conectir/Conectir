a7f8804c71224d1df7003c4af6e2d68910669c65
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-09-09 17:34:53 -0500

    easy changes added to publish on gh-pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Auth from './containers/Auth'
 import Header from './components/Header'
 import Equipment from './containers/Equipment'
 
-import { Switch, Route } from 'wouter'
+import { Switch, Route, Router } from 'wouter'
 import { UserProvider } from './contexts/User'
 
 import './assets/css/reset.scss';
@@ -12,16 +12,18 @@ import './assets/css/vars.scss';
 
 function App() {
   return (
-    <div className="App">
-      <UserProvider>
-        <Header />
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/equipment" component={Equipment} />
-          <Route path="/log" component={Auth} />
-        </Switch>
-      </UserProvider>
-    </div>
+    <Router base="/Conectir">
+      <div className="App">
+        <UserProvider>
+          <Header />
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/equipment" component={Equipment} />
+            <Route path="/log" component={Auth} />
+          </Switch>
+        </UserProvider>
+      </div>
+    </Router>
   );
 }
 
